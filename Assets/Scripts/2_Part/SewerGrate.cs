707093701e9f1ca4448555d4e7df3c5d986045a2
Aziz <zhaparovaziz03@gmail.com>
2025-06-15 15:24:33 +0600

    elevator animations

--- Assets/Scripts/2_Part/SewerGrate.cs
+++ Assets/Scripts/2_Part/SewerGrate.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SewerGrate : MonoBehaviour
 {
     [SerializeField] private Animator openSewerGrate;
+    [SerializeField] private bool isTransitonTo_3Level;
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +25,18 @@ public class SewerGrate : MonoBehaviour
         if (other.CompareTag("Player") && (Input.GetKeyDown(KeyCode.E)))
         {
             openSewerGrate.SetTrigger("OpenDoor");
+
+            if (isTransitonTo_3Level)
+            {
+                openSewerGrate.SetTrigger("CloseDoor");
+                StartCoroutine(WaitTime());
+            }
         }
+    }
+
+    private IEnumerator WaitTime()
+    {
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene(3);
     }
 }
