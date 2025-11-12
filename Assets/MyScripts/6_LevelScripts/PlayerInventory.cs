29405fff6c1d40cf6df251a40a9c1ae0029ff832
Zh-AZ <zhaparovaziz03@gmail.com>
2025-11-13 00:26:02 +0600

    Open doors triggers

--- Assets/MyScripts/6_LevelScripts/PlayerInventory.cs
+++ Assets/MyScripts/6_LevelScripts/PlayerInventory.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class PlayerInventory : MonoBehaviour
 {
     protected bool hasKey = true;
+    protected bool hasElectricity = true;
+    protected bool hasCanisterFuel = true;
+    protected bool hasSkeletonKeys = true;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
